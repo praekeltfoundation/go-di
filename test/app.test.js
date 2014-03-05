248c866cb1f4a0be3c286d15df5a832c706e22c1
retrorabbitFS <fscheffer@retrorabbit.co.za>
2014-03-05 16:29:38 +0200

    Language menu added (@justinvdm)

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,7 @@
 var vumigo = require('vumigo_v02');
 var app = require('../lib/app');
+var Choice = vumigo.states.Choice;
+var ChoiceState = vumigo.states.ChoiceState;
 var GoDiApp = app.GoDiApp;
 var AppTester = vumigo.AppTester;
 
@@ -19,15 +21,42 @@ describe("app", function() {
         });
 
         describe("when the user starts a session", function() {
-            it("should say hello", function() {
+            it("should ask them to choose a language ", function() {
                 return tester
                     .start()
                     .check.interaction({
                         state: 'states:start',
-                        reply: 'Hello :)'
+                        reply: ['your language:',
+                                '1. English',
+                                '2. Afrikaans',
+                                '3. Zulu',
+                                '4. Xhosa',
+                                '5. Sotho'
+                        ].join('\n')
                     })
                     .run();
             });
         });
+
+        describe("when the user has submitted their language", function() {
+            it("should set the user's language",function() {
+               return tester
+                   .setup.user.state('states:start')
+                   .input('1')
+                   .check.user.properties({lang: 'en'})
+                   .run();
+            });
+
+            it("should show them the address menu",function() {
+                return tester
+                    .setup.user.state('states:start')
+                    .input('1')
+                    .check.interaction({
+                        state:'states:address',
+                        reply:'To be completed'
+                    });
+            });
+        });
+
     });
 });
