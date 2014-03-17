f5b5e9e716a726738b293315f4877b03e6c29d8d
retrorabbitFS <fscheffer@retrorabbit.co.za>
2014-03-17 10:09:31 +0200

    Modified to provide user with a list of locations

--- test/fixtures.js
+++ test/fixtures.js
@@ -11,20 +11,50 @@ module.exports = function() {
             },
             "response": {
                 "code": "200",
-                "data": {
-                    "province": "GT",
-                    "coords": [
-                        "27.97532",
-                        "-26.02674"
-                    ],
-                    "now31": "0:00:00.718923",
-                    "now32": "0:00:00.015597",
-                    "address": "21 conduit street, johannesburg north",
-                    "ward": "79400094",
-                    "ward_no": "94",
-                    "municipality": "City of Johannesburg",
-                    "now21": "0:00:00.703326"
-                }
+                "data": [
+                    {
+                        "province": "GT",
+                        "wards_no": 94,
+                        "coords": [
+                            -26.02674,
+                            27.97532
+                        ],
+                        "now31": "0:00:01.173270",
+                        "now32": "0:00:00.041757",
+                        "address": "21 Conduit Street, Randburg 2188, South Africa",
+                        "ward": "79400094",
+                        "municipality": "City of Johannesburg",
+                        "now21": "0:00:01.131513"
+                    },
+                    {
+                        "province": "GT",
+                        "wards_no": 104,
+                        "coords": [
+                            -26.0701361,
+                            27.9946541
+                        ],
+                        "now31": "0:00:01.177459",
+                        "now32": "0:00:00.045946",
+                        "address": "21 Conduit Street, Sandton 2191, South Africa",
+                        "ward": "79400104",
+                        "municipality": "City of Johannesburg",
+                        "now21": "0:00:01.131513"
+                    },
+                    {
+                        "province": "GT",
+                        "wards_no": 104,
+                        "coords": [
+                            -26.0874853,
+                            28.0094074
+                        ],
+                        "now31": "0:00:01.189634",
+                        "now32": "0:00:00.058121",
+                        "address": "21 Conduit Street, Randburg 2194, South Africa",
+                        "ward": "79400104",
+                        "municipality": "City of Johannesburg",
+                        "now21": "0:00:01.131513"
+                    }
+                ]
             }
         },
         {
