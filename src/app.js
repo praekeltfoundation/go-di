e4091b953186e9125fb024ca088894eaa1b77de7
retrorabbitFS <fscheffer@retrorabbit.co.za>
2014-03-28 12:30:32 +0200

    Added tests for answerwin quiz and fixed copy errors

--- src/app.js
+++ src/app.js
@@ -130,8 +130,6 @@ di.app = function() {
                 });
         };
 
-
-
         self.get_unique_users = function() {
             return self.im
                 .api_request('messagestore.count_inbound_uniques',{})
@@ -319,7 +317,7 @@ di.app = function() {
             return new MenuState(name, {
                 question: $('Welcome to VIP!'),
                 choices:[
-                    new Choice('states:answerwin',$('Answer & win!')),
+                    new Choice('states:answerwin:gender',$('Answer & win!')),
                     new Choice(self.quizzes.vip.get_next_quiz_state(),$('VIP Quiz')),
                     new Choice('states:report',$('Report an Election Activity')),
                     new Choice('states:results',$('View VIP results...')),
@@ -330,10 +328,104 @@ di.app = function() {
             });
         });
 
-        self.states.add('states:answerwin',function(name) {
-            return new EndState(name, {
-                text: $('To be continued'),
-                next: 'states:start'
+        self.states.add('states:answerwin:gender',function(name) {
+            return new ChoiceState(name, {
+                question: $('I am...'),
+                choices: [
+                    new Choice('male',$('Male')),
+                    new Choice('female',$('Female')),
+                ],
+                next: function(choice) {
+                    self.contact.extra.answerwin_question_gender = choice.value;
+                    self.contact.extra.it_answerwin_question_gender = self.get_date_string();
+                    return self
+                        .im.contacts.save(self.contact)
+                        .then(function() {
+                            return 'states:answerwin:age';
+                        });
+                }
+            });
+        });
+
+        self.states.add('states:answerwin:age',function(name) {
+            return new PaginatedChoiceState(name, {
+                question: $('How old are you?'),
+                choices: [
+                    new Choice('u14',$('u14')),
+                    new Choice('15-19',$('15-19')),
+                    new Choice('20-29',$('20-29')),
+                    new Choice('30-39',$('30-39')),
+                    new Choice('40-49',$('40-49')),
+                    new Choice('50-59',$('50-59')),
+                    new Choice('60-69',$('60-69')),
+                    new Choice('70-79',$('70-79')),
+                    new Choice('80+',$('80+'))
+                ],
+                characters_per_page: 160,
+                options_per_page: 5,
+                next: function(choice) {
+                    self.contact.extra.answerwin_question_age = choice.value;
+                    self.contact.extra.it_answerwin_question_age = self.get_date_string();
+                    return self
+                        .im.contacts.save(self.contact)
+                        .then(function() {
+                            return 'states:answerwin:2009election';
+                        });
+                }
+            });
+        });
+
+        self.states.add('states:answerwin:2009election',function(name) {
+            return new ChoiceState(name, {
+                question: $('Did you vote in the 2009 election?'),
+                choices: [
+                    new Choice('yes',$('Yes')),
+                    new Choice('no_not_registered',$('No, could not/was not registered')),
+                    new Choice('no_didnt_want_to',$('No, did not want to')),
+                    new Choice('no_other',$('No, other')),
+                    new Choice('skip',$('Skip'))
+                ],
+                next: function(choice) {
+                    self.contact.extra.answerwin_question_2009election = choice.value;
+                    self.contact.extra.it_answerwin_question_2009election = self.get_date_string();
+                    return self
+                        .im.contacts.save(self.contact)
+                        .then(function() {
+                            return 'states:answerwin:race';
+                        });
+                }
+            });
+        });
+
+        self.states.add('states:answerwin:race',function(name) {
+            return new ChoiceState(name, {
+                question: $('I am...'),
+                choices: [
+                    new Choice('black_african',$('Black African')),
+                    new Choice('coloured',$('Coloured')),
+                    new Choice('indian_or_asian',$('Indian/Asian')),
+                    new Choice('white',$('White')),
+                    new Choice('other',$('Other')),
+                    new Choice('skip',$('Skip')),
+                ],
+                next: function(choice) {
+                    self.contact.extra.answerwin_question_race = choice.value;
+                    self.contact.extra.it_answerwin_question_race = self.get_date_string();
+                    return self
+                        .im.contacts.save(self.contact)
+                        .then(function() {
+                            return 'states:answerwin:thankyou';
+                        });
+                }
+            });
+        });
+
+        self.states.add('states:answerwin:thankyou',function(name) {
+            return new MenuState(name, {
+                question: $('Thank you for telling VIP a bit more about yourself! Your airtime will be sent to you shortly!'),
+                choices: [
+                    new Choice('states:menu',$('Main Menu'))
+                ]
             });
         });
 
---
+++ test/answerwin.test.js
@@ -0,0 +1,259 @@
+var vumigo = require('vumigo_v02');
+var AppTester = vumigo.AppTester;
+var assert = require('assert');
+
+describe("app", function() {
+    describe.only("Answer & Win Quiz", function() {
+
+        var app;
+        var tester;
+
+        beforeEach(function() {
+            app = new di.app.GoDiApp();
+
+            tester = new AppTester(app,{
+                api: {http: {default_encoding: 'json'}}
+            })
+            .setup.char_limit(180);
+
+            app.get_date = function() {
+                var d = new Date();
+                d.setHours(0,0,0,0);
+                return d;
+            };
+
+            tester
+                .setup.config.app({
+                    name: 'test_app',
+                    endpoints: {
+                        "sms": {"delivery_class": "sms"}
+                    }
+                })
+                .setup(function(api) {
+                    api.contacts.add( {
+                        msisdn: '+273123',
+                        extra : {
+                            is_registered: 'true',
+                            register_sms_sent: 'true'
+                        }
+                    });
+                });
+        });
+
+        describe("when the user chooses to do the Answer & Win quiz from the main menu",function() {
+            it("should take them to the gender question of the quiz",function() {
+                    return tester
+                        .setup.user.addr("+273123")
+                        .setup.user.state('states:menu')
+                        .input('1')
+                        .check.interaction({
+                            state: 'states:answerwin:gender',
+                            reply: [
+                                'I am...',
+                                '1. Male',
+                                '2. Female'
+                            ].join('\n')
+                        }).run();
+            });
+        });
+
+        describe("when the user has answered the gender question",function() {
+            beforeEach(function() {
+                return tester
+                    .setup.user.addr("+273123")
+                    .setup.user.state('states:answerwin:gender')
+                    .input('1');
+            });
+
+            it("should save their answer to the gender question",function() {
+                return tester
+                    .check(function(api){
+                        var contact = api.contacts.store[0];
+                        assert.equal(contact.extra.answerwin_question_gender,"male");
+                        assert.equal(contact.extra.it_answerwin_question_gender,app.get_date_string());
+                    });
+            });
+
+            it("should take them to the age question",function() {
+                return tester
+                    .check.interaction({
+                        state: 'states:answerwin:age',
+                        reply: [
+                            'How old are you?',
+                            '1. u14',
+                            '2. 15-19',
+                            '3. 20-29',
+                            '4. 30-39',
+                            '5. 40-49',
+                            '6. More'
+                        ].join('\n')
+                    }).run();
+            });
+        });
+
+        describe("when the user has answered the age question",function() {
+            beforeEach(function() {
+                return tester
+                    .setup.user.addr("+273123")
+                    .setup.user.state('states:answerwin:age');
+            });
+
+            it("should save their answer to the age question",function() {
+                return tester
+                    .input("1")
+                    .check(function(api){
+                        var contact = api.contacts.store[0];
+                        assert.equal(contact.extra.answerwin_question_age,"u14");
+                        assert.equal(contact.extra.it_answerwin_question_age,app.get_date_string());
+                    });
+            });
+
+            it("should take them to the 2009 election question ",function() {
+                return tester
+                    .input("1")
+                    .check.interaction({
+                        state: 'states:answerwin:2009election',
+                        reply: [
+                            'Did you vote in the 2009 election?',
+                            '1. Yes',
+                            '2. No, could not/was not registered',
+                            '3. No, did not want to',
+                            '4. No, other',
+                            '5. Skip'
+                        ].join('\n')
+                    }).run();
+            });
+
+            describe("if they select to see more ages ",function() {
+                it("should take them to see older ages ",function() {
+                    return tester
+                        .input("6")
+                        .check.interaction({
+                            state: 'states:answerwin:age',
+                            reply: [
+                                'How old are you?',
+                                '1. 50-59',
+                                '2. 60-69',
+                                '3. 70-79',
+                                '4. 80+',
+                                '5. Back'
+                            ].join('\n')
+                        }).run();
+                });
+            });
+        });
+
+        describe("when the user has answered the 2009 election question",function() {
+            beforeEach(function() {
+                return tester
+                    .setup.user.addr("+273123")
+                    .setup.user.state('states:answerwin:2009election')
+                    .input('1');
+            });
+
+            it("should save their answer to the  2009 election question",function() {
+                return tester
+                    .check(function(api){
+                        var contact = api.contacts.store[0];
+                        assert.equal(contact.extra.answerwin_question_2009election,"yes");
+                        assert.equal(contact.extra.it_answerwin_question_2009election,app.get_date_string());
+                    });
+            });
+
+            it("should take them to the race question",function() {
+                return tester
+                    .check.interaction({
+                        state: 'states:answerwin:race',
+                        reply: [
+                            'I am...',
+                            '1. Black African',
+                            '2. Coloured',
+                            '3. Indian/Asian',
+                            '4. White',
+                            '5. Other',
+                            '6. Skip'
+                        ].join('\n')
+                    }).run();
+            });
+        });
+
+        describe("when the user has answered the 2009 election question",function() {
+            beforeEach(function() {
+                return tester
+                    .setup.user.addr("+273123")
+                    .setup.user.state('states:answerwin:2009election')
+                    .input('1');
+            });
+
+            it("should save their answer to the  2009 election question",function() {
+                return tester
+                    .check(function(api){
+                        var contact = api.contacts.store[0];
+                        assert.equal(contact.extra.answerwin_question_2009election,"yes");
+                        assert.equal(contact.extra.it_answerwin_question_2009election,app.get_date_string());
+                    });
+            });
+
+            it("should take them to the race question",function() {
+                return tester
+                    .check.interaction({
+                        state: 'states:answerwin:race',
+                        reply: [
+                            'I am...',
+                            '1. Black African',
+                            '2. Coloured',
+                            '3. Indian/Asian',
+                            '4. White',
+                            '5. Other',
+                            '6. Skip'
+                        ].join('\n')
+                    }).run();
+            });
+        });
+
+        describe("when the user has answered the race question",function() {
+            beforeEach(function() {
+                return tester
+                    .setup.user.addr("+273123")
+                    .setup.user.state('states:answerwin:race')
+                    .input('1');
+            });
+
+            it("should save their answer to the race question",function() {
+                return tester
+                    .check(function(api){
+                        var contact = api.contacts.store[0];
+                        assert.equal(contact.extra.answerwin_question_race,"black_african");
+                        assert.equal(contact.extra.it_answerwin_question_race,app.get_date_string());
+                    });
+            });
+
+            it("should take them to the thank you question",function() {
+                return tester
+                    .check.interaction({
+                        state: 'states:answerwin:thankyou',
+                        reply: [
+                            'Thank you for telling VIP a bit more about yourself! Your airtime will be sent to you shortly!',
+                            '1. Main Menu'
+                        ].join('\n')
+                    }).run();
+            });
+        });
+
+        describe("when the user has selected to go to the main menu",function() {
+            beforeEach(function() {
+                return tester
+                    .setup.user.addr("+273123")
+                    .setup.user.state('states:answerwin:thankyou')
+                    .input('1');
+            });
+
+            it("should take them to the main menu",function() {
+                return tester
+                    .check.interaction({
+                        state: 'states:menu'
+                    }).run();
+            });
+        });
+    });
+});
