3105fc022c1c797521903ad1f82c60fb61dd3255
retrorabbitFS <fscheffer@retrorabbit.co.za>
2014-03-28 13:32:03 +0200

    Moved stray .only Added translation for endpoints

--- src/answerwinquiz.js
+++ src/answerwinquiz.js
@@ -18,7 +18,6 @@ di.quiz.answerwin = function() {
                     return self.incr_quiz_metrics();
                 })
                 .then(function() {
-                    console.log(self.next);
                     return self.construct_state_name(next);
                 });
         };
