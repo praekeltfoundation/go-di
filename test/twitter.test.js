e1931f8ac53b859477fcf1093e0ff8323e686d74
retrorabbitFS <fscheffer@retrorabbit.co.za>
2014-03-31 14:11:29 +0200

    Tests for saving phone number

--- test/twitter.test.js
+++ test/twitter.test.js
@@ -3,7 +3,7 @@ var AppTester = vumigo.AppTester;
 var assert = require('assert');
 
 describe("app", function() {
-    describe("G+ Quiz test", function() {
+    describe("Twitter Quiz test", function() {
 
         var app;
         var tester;
@@ -14,7 +14,7 @@ describe("app", function() {
             tester = new AppTester(app,{
                 api: {
                     http: {default_encoding: 'json'},
-                    delivery_class: 'gplus'
+                    delivery_class: 'twitter'
                 }
             })
                 .setup.char_limit(180);
@@ -31,11 +31,11 @@ describe("app", function() {
                     endpoints: {
                         "sms": {"delivery_class": "sms"}
                     },
-                    "delivery_class" : "u"
+                    delivery_class: 'twitter'
                 })
                 .setup(function(api) {
                     api.contacts.add( {
-                        msisdn: '+273123',
+                        twitter_handle: "@test",
                         extra : {
                             is_registered: 'true',
                             register_sms_sent: 'true'
@@ -43,3 +43,62 @@ describe("app", function() {
                     });
                 });
         });
+
+        describe("when the user has answered the race question",function() {
+            beforeEach(function() {
+                return tester
+                    .setup.user.addr("@test")
+                    .setup.user.state('states:quiz:answerwin:race')
+                    .input('1');
+            });
+
+            it("should save their answer to the race question",function() {
+                return tester
+                    .check(function(api){
+                        var contact = api.contacts.store[0];
+                        assert.equal(contact.extra.answerwin_question_race,"black_african");
+                        assert.equal(contact.extra.it_answerwin_question_race,app.get_date_string());
+                    });
+            });
+
+            it("should take them to the phone number question",function() {
+                return tester
+                    .check.interaction({
+                        state: 'states:quiz:answerwin:phonenumber',
+                        reply: [
+                            'Please give us your cellphone number so we can send you your airtime!'
+                        ].join('\n')
+                    }).run();
+            });
+        });
+
+        describe.only("when the user has provided there cell phone number",function() {
+            beforeEach(function() {
+                return tester
+                    .setup.user.addr("@test")
+                    .setup.user.state('states:quiz:answerwin:phonenumber')
+                    .input('0729042520');
+            });
+
+            it("should save their cell phone number",function() {
+                return tester
+                    .check(function(api){
+                        var contact = api.contacts.store[0];
+                        assert.equal(contact.msisdn,"+27729042520");
+                    });
+            });
+
+            it("should take them to the thank you question",function() {
+                return tester
+                    .check.interaction({
+                        state: 'states:quiz:answerwin:thankyou',
+                        reply: [
+                            'Thank you for telling VIP a bit more about yourself! Your airtime will be sent to you shortly!',
+                            '1. Main Menu'
+                        ].join('\n')
+                    }).run();
+            });
+        });
+
+    });
+});
