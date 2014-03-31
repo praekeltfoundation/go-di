28a1c2346bd2d82208d4b4362f1654f1967ba96a
retrorabbitFS <fscheffer@retrorabbit.co.za>
2014-03-31 11:13:58 +0200

    Added test file for gplus

--- test/gplus.test.js
+++ test/gplus.test.js
@@ -1,3 +1,45 @@
-/**
- * Created by Jade on 2014/03/31.
- */
+var vumigo = require('vumigo_v02');
+var AppTester = vumigo.AppTester;
+var assert = require('assert');
+
+describe("app", function() {
+    describe("G+ Quiz test", function() {
+
+        var app;
+        var tester;
+
+        beforeEach(function() {
+            app = new di.app.GoDiApp();
+
+            tester = new AppTester(app,{
+                api: {
+                    http: {default_encoding: 'json'},
+                    delivery_class: 'gplus'
+                }
+            })
+                .setup.char_limit(180);
+
+            app.get_date = function() {
+                var d = new Date();
+                d.setHours(0,0,0,0);
+                return d;
+            };
+
+            tester
+                .setup.config.app({
+                    name: 'test_app',
+                    endpoints: {
+                        "sms": {"delivery_class": "sms"}
+                    },
+                    "delivery_class" : "u"
+                })
+                .setup(function(api) {
+                    api.contacts.add( {
+                        msisdn: '+273123',
+                        extra : {
+                            is_registered: 'true',
+                            register_sms_sent: 'true'
+                        }
+                    });
+                });
+        });
