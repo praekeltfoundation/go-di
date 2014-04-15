1f381d4c480e8552fde169590eb2bd5ac40719f3
retrorabbitFS <fscheffer@retrorabbit.co.za>
2014-04-15 11:24:14 +0200

    Modified to use 'states:noop'

--- src/pushmessage.states.js
+++ src/pushmessage.states.js
@@ -4,23 +4,36 @@ di.push_message_states = function() {
     var AppStates = vumigo.app.AppStates;
     var PushMessageApi = di.push_message.PushMessageApi;
 
-    var PushMessageStates = AppStates.extend(function(self,app,opts) {
+    var DiAppStates  = AppStates.extend(function(self,app,opts) {
         AppStates.call(self, app);
         self.pushmessage = new PushMessageApi(app.im,app);
         var create =  self.create;
 
         self.create = function(name,opts) {
-            //Check if dummy message
-            var msg = app.im.msg;
-            if (_.isUndefined(msg.inbound_push_trigger)) {
-                return create(name,opts);
-            } else {
-                return self.pushmessage.send_push_message();
+            if (!self.app.im.msg.inbound_push_trigger) {
+                return create(name, opts);
             }
+
+            return !self.should_push()
+                ? create('states:noop')
+                : self.get_push();
+        };
+
+        self.should_push = function() {
+            return self.pushmessage.should_send_push();
+        };
+
+        self.get_push = function() {
+            self.pushmessage
+                .rerandomize_week_day()
+                .then(function() {
+                    var state = self.pushmessage.get_push_message();
+                    return create('states:push:start',state);
+                });
         };
     });
 
     return {
-        PushMessageStates: PushMessageStates
+        DiAppStates : DiAppStates
     };
 }();
